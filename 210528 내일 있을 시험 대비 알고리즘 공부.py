d43b2b4b1d88f394edce656e63848c423b99bf89
JEON <fogmaster651666@naver.com>
2021-05-28 21:17:32 +0900

    algorithm studying

---
+++ 210528 내일 있을 시험 대비 알고리즘 공부.py
@@ -0,0 +1,117 @@
+#큰 수의 법칙
+#간만에 풀었는데 정답~
+'''
+n, m, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+
+result = 0
+
+arr.sort(reverse = True)
+
+big1 = arr[0]
+big2 = arr[1]
+
+while m > 0:
+    for i in range(k):
+        result += big1
+        m -= 1
+    result += big2
+    m -= 1
+
+print(result)
+'''
+
+#숫자 카드 게임
+#간만에 풀었는데 정답~
+'''
+n, m = map(int, input().split())
+
+li = []
+
+result = 0
+
+for i in range(n):
+    data1 = list(map(int, input().split()))
+    li.append(min(data1))
+
+result = max(li)
+
+print(result)
+'''
+#1이 될 때까지
+#간만에 풀었는데 정답~
+'''
+n, k = map(int, input().split())
+result = 0
+
+while True:
+    target = (n//k) * k
+    result = result + n - target
+    n = target
+    if n < k:
+        break
+    n = n // k
+    result += 1
+
+result = result + (n -1)
+print(result)
+'''
+#모험가 길드
+#간만에 풀었는데 정답~
+'''
+n = int(input())
+x = list(map(int, input().split()))
+
+x.sort()
+
+result = 0
+count = 0
+
+for i in x:
+    count += 1
+    if count == i:
+        result += 1
+        count = 0
+
+print(result)
+'''
+#곱하기 혹은 더하기
+#간만에 풀었는데 정답~
+'''
+n = input()
+num = []
+count = 1
+
+for i in n:
+    num.append(int(i))
+
+for j in range(len(num)):
+    if num[j] != 0 and num[j] != 1:
+        count *= num[j]
+    else:
+        count += num[j]
+
+print(count)
+'''
+#문자열 뒤집기
+#간만에 풀었는데 정답~
+'''
+s = input()
+data0 = 0
+data1 = 0
+
+if s[0] == '1':
+    data1 += 1
+elif s[0] == '1':
+    data0 += 1
+
+for i in range(1, len(s)-1):
+    if s[i] != s[i+1]:
+        if s[i] == '0':
+            data0 += 1
+        elif s[i] == '1':
+            data1 += 1
+
+print(min(data1, data0))
+'''
